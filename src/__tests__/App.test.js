17cab77968c6a4524bed957dd2f3141f9fc070d1
Akash Agarwal <akash191095@gmail.com>
2020-10-13 10:56:03 +0530

    test register page renders

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "../components/App";
 import AppProviders from "../components/AppProviders";
 
@@ -15,4 +16,13 @@ test("renders login", () => {
   const { getByText } = render(withProviders(App));
   const loginButton = getByText(/login/i);
   expect(loginButton).toBeInTheDocument();
+});
+
+test("renders register page", () => {
+  const { getByText } = render(withProviders(App));
+  const signUpLink = getByText(/register/i);
+  expect(signUpLink).toBeInTheDocument();
+  userEvent.click(signUpLink);
+  const loginLink = getByText(/go to login/i);
+  expect(loginLink).toBeInTheDocument();
 });
